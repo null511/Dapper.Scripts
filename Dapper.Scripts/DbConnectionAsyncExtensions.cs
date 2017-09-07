5ebb1382c349a3a56707271ef7bc8e8470487174
JoshuaMiller <admin@joshuamiller.info>
2017-09-06 22:32:50 -0400

    async fix

--- Dapper.Scripts/DbConnectionAsyncExtensions.cs
+++ Dapper.Scripts/DbConnectionAsyncExtensions.cs
@@ -38,61 +38,61 @@ namespace Dapper.Scripts
             return await connection.QueryAsync(sql, param, transaction, commandTimeout, commandType);
         }
 
-        public static async Task<IEnumerable<T>> QueryScriptAsync<T>(this SqlScriptConnection connection, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        public static async Task<IEnumerable<T>> QueryScriptAsync<T>(this ISqlScriptConnection connection, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
             var sql = connection.GetScriptSql(key, param);
             return await connection.QueryAsync<T>(sql, param, transaction, commandTimeout, commandType);
         }
 
-        public static async Task<dynamic> QueryFirstScriptAsync(this SqlScriptConnection connection, Type type, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        public static async Task<dynamic> QueryFirstScriptAsync(this ISqlScriptConnection connection, Type type, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
             var sql = connection.GetScriptSql(key, param);
             return await connection.QueryFirstAsync(type, sql, param, transaction, commandTimeout, commandType);
         }
 
-        public static async Task<T> QueryFirstScriptAsync<T>(this SqlScriptConnection connection, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        public static async Task<T> QueryFirstScriptAsync<T>(this ISqlScriptConnection connection, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
             var sql = connection.GetScriptSql(key, param);
             return await connection.QueryFirstAsync<T>(sql, param, transaction, commandTimeout, commandType);
         }
 
-        public static async Task<dynamic> QueryFirstOrDefaultScriptAsync(this SqlScriptConnection connection, Type type, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        public static async Task<dynamic> QueryFirstOrDefaultScriptAsync(this ISqlScriptConnection connection, Type type, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
             var sql = connection.GetScriptSql(key, param);
             return await connection.QueryFirstOrDefaultAsync(type, sql, param, transaction, commandTimeout, commandType);
         }
 
-        public static async Task<T> QueryFirstOrDefaultScriptAsync<T>(this SqlScriptConnection connection, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        public static async Task<T> QueryFirstOrDefaultScriptAsync<T>(this ISqlScriptConnection connection, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
             var sql = connection.GetScriptSql(key, param);
             return await connection.QueryFirstOrDefaultAsync<T>(sql, param, transaction, commandTimeout, commandType);
         }
 
-        public static async Task<SqlMapper.GridReader> QueryMultipleScriptAsync(this SqlScriptConnection connection, string key, object param = null, IDbTransaction transaction = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null)
+        public static async Task<SqlMapper.GridReader> QueryMultipleScriptAsync(this ISqlScriptConnection connection, string key, object param = null, IDbTransaction transaction = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null)
         {
             var sql = connection.GetScriptSql(key, param);
             return await connection.QueryMultipleAsync(sql, param, transaction, commandTimeout, commandType);
         }
 
-        public static async Task<dynamic> QuerySingleScriptAsync(this SqlScriptConnection connection, Type type, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        public static async Task<dynamic> QuerySingleScriptAsync(this ISqlScriptConnection connection, Type type, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
             var sql = connection.GetScriptSql(key, param);
             return await connection.QuerySingleAsync(type, sql, param, transaction, commandTimeout, commandType);
         }
 
-        public static async Task<T> QuerySingleScriptAsync<T>(this SqlScriptConnection connection, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        public static async Task<T> QuerySingleScriptAsync<T>(this ISqlScriptConnection connection, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
             var sql = connection.GetScriptSql(key, param);
             return await connection.QuerySingleAsync<T>(sql, param, transaction, commandTimeout, commandType);
         }
 
-        public static async Task<dynamic> QuerySingleOrDefaultScriptAsync(this SqlScriptConnection connection, Type type, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        public static async Task<dynamic> QuerySingleOrDefaultScriptAsync(this ISqlScriptConnection connection, Type type, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
             var sql = connection.GetScriptSql(key, param);
             return await connection.QuerySingleOrDefaultAsync(type, sql, param, transaction, commandTimeout, commandType);
         }
 
-        public static async Task<T> QuerySingleOrDefaultScriptAsync<T>(this SqlScriptConnection connection, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        public static async Task<T> QuerySingleOrDefaultScriptAsync<T>(this ISqlScriptConnection connection, string key, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
             var sql = connection.GetScriptSql(key, param);
             return await connection.QuerySingleOrDefaultAsync<T>(sql, param, transaction, commandTimeout, commandType);
