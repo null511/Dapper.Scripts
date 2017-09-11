5244989288033764626f5bfadd7ccda05bd71299
JoshuaMiller <admin@joshuamiller.info>
2017-09-11 14:10:42 -0400

    public sqlconn interface

--- Dapper.Scripts/Connection/ISqlScriptConnection.cs
+++ Dapper.Scripts/Connection/ISqlScriptConnection.cs
@@ -1,9 +1,12 @@
 ﻿using System.Data;
+using System.Data.SqlClient;
 
 namespace Dapper.Scripts.Connection
 {
     public interface ISqlScriptConnection : IDbConnection
     {
+        SqlConnection SqlConnection {get;}
+
         string GetScriptSql(string key, object param = null);
     }
 }
