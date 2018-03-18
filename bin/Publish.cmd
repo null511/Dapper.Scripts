067eba347b4d6821fa0188da520c195156fdfa04
JoshuaMiller <admin@joshuamiller.info>
2018-03-18 19:29:31 -0400

    include bin

---
+++ bin/Publish.cmd
@@ -0,0 +1,3 @@
+if exist "Dapper.Scripts\bin\Package\" rmdir /Q /S "Dapper.Scripts\bin\Package"
+nuget pack "Dapper.Scripts\Dapper.Scripts.csproj" -OutputDirectory "Dapper.Scripts\bin\Package" -Build -Prop "Configuration=Release;Platform=AnyCPU"
+nuget push "Dapper.Scripts\bin\Package\*.nupkg" -Source "https://www.nuget.org/api/v2/package" -NonInteractive
