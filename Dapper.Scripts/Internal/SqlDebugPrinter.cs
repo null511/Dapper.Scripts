0020a6b3c9ae3a96f7bf49bef3d5233ecbe6c1a2
JoshuaMiller <admin@joshuamiller.info>
2018-07-15 13:46:13 -0400

    debug printer; sql builder

---
+++ Dapper.Scripts/Internal/SqlDebugPrinter.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Dapper.Scripts.Internal
+{
+    internal static class SqlDebugPrinter
+    {
+        private static readonly Type[] stringTypes = {
+            typeof(char),
+            typeof(string),
+            typeof(DateTime),
+        };
+
+
+        public static string GetDebugString(string sql, object parameters)
+        {
+            var _text = sql;
+            var _params = GetParameterCollection(parameters);
+
+            foreach (var paramName in _params.Keys) {
+                var paramValue = _params[paramName];
+                var displayValue = GetParameterDisplayValue(paramValue);
+
+                var eParamName = Regex.Escape(paramName);
+                var pattern = $"[^\\w]({eParamName})[^\\w]";
+                var matches = Regex.Matches(_text, pattern, RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+                for (var i = matches.Count-1; i >= 0; i--) {
+                    var match = matches[i];
+
+                    var g = match.Groups[1];
+
+                    _text = ReplaceTextSegment(_text, g.Index, g.Length, displayValue);
+                }
+            }
+
+            return _text;
+        }
+
+        public static IDictionary<string, object> GetParameterCollection(object parameters)
+        {
+            if (parameters == null) return null;
+
+            if (parameters is IDictionary<string, object> paramDictionary)
+                return paramDictionary;
+
+            var paramType = parameters.GetType();
+            var paramList = paramType.GetProperties();
+            paramDictionary = new Dictionary<string, object>();
+
+            foreach (var param in paramList) {
+                var paramValue = param.GetValue(parameters);
+                paramDictionary[param.Name] = paramValue;
+            }
+
+            return paramDictionary;
+        }
+
+        private static string ReplaceTextSegment(string text, int start, int length, string replacementText)
+        {
+            var text_pre = text.Substring(0, start);
+            var text_post = text.Substring(start + length);
+            return string.Concat(text_pre, replacementText, text_post);
+        }
+
+        private static string GetParameterDisplayValue(object value)
+        {
+            if (value == null) return "null";
+
+            var paramType = value.GetType();
+
+            if (paramType == typeof(DateTime))
+                return $"'{(DateTime)value:yyyy-MM-dd HH:mm:ss.fff}'";
+
+            if (stringTypes.Contains(paramType))
+                return $"'{value}'";
+
+            if (value is IEnumerable collection) {
+                var values = collection.OfType<object>()
+                    .Select(GetParameterDisplayValue);
+
+                return $"({string.Join(", ", values)})";
+            }
+
+            return value.ToString();
+        }
+    }
+}
---
+++ Dapper.Scripts/SqlBuilder.cs
@@ -0,0 +1,97 @@
+﻿using Dapper.Scripts.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dapper.Scripts
+{
+    /// <summary>
+    /// Builds a SQL statement by appending text and auto-indexed parameters.
+    /// Designed for batching SQL statements.
+    /// </summary>
+    public class SqlBuilder
+    {
+        private readonly StringBuilder builderText;
+        private readonly Dictionary<string, object> paramList;
+
+        /// <summary>
+        /// Gets the SQL text that has been appended to the builder.
+        /// </summary>
+        public string Text => builderText.ToString();
+
+        /// <summary>
+        /// Gets a collection of SQL parameters that have been added to the builder.
+        /// </summary>
+        public IReadOnlyDictionary<string, object> Parameters => paramList;
+
+
+        /// <summary>
+        /// Creates a new and empty SQL builder instance.
+        /// </summary>
+        public SqlBuilder()
+        {
+            builderText = new StringBuilder();
+            paramList = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Appends SQL text to the builder instance.
+        /// </summary>
+        /// <param name="text">The SQL text to be appended to the builder.</param>
+        public void Append(string text)
+        {
+            builderText.Append(text);
+        }
+
+        /// <summary>
+        /// Appends SQL text to the builder instance,
+        /// followed by the default line terminator.
+        /// </summary>
+        /// <param name="text">The SQL text to be appended to the builder.</param>
+        public void AppendLine(string text = null)
+        {
+            builderText.AppendLine(text);
+        }
+
+        /// <summary>
+        /// Generates and appends a uniquely named SQL parameter to the collection.
+        /// </summary>
+        /// <param name="value">The value of the parameter.</param>
+        /// <returns>The unique parameter name. ex: @__pN</returns>
+        public string AppendIndexedParam(object value)
+        {
+            var index = paramList.Count;
+            var paramName = $"@__p{index}_";
+            paramList[paramName] = value;
+            return paramName;
+        }
+
+        /// <summary>
+        /// Appends a named SQL parameter to the collection.
+        /// </summary>
+        /// <param name="name">The name of the SQL parameter.</param>
+        /// <param name="value">The value of the SQL parameter</param>
+        public void AppendParam(string name, object value)
+        {
+            if (paramList.ContainsKey(name)) throw new ApplicationException($"The named parameter '{name}' already exists!");
+
+            paramList[name] = value;
+        }
+
+        /// <summary>
+        /// Gets a string containing the SQL text of this instance,
+        /// with all parameters injected as text. Useful for debugging.
+        /// </summary>
+        public string ToDebugString()
+        {
+            var sql = builderText.ToString();
+            return SqlDebugPrinter.GetDebugString(sql, paramList);
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return builderText.ToString();
+        }
+    }
+}
