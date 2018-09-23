a05530cc4eff5890ad8f375169d6a8d98fc22847
JoshuaMiller <admin@joshuamiller.info>
2018-09-23 12:47:35 -0400

    more unit tests

---
+++ Dapper.Scripts.Tests/UnitTests/ScriptCollectionTests.cs
@@ -0,0 +1,75 @@
+﻿using Dapper.Scripts.Collection;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Dapper.Scripts.Tests.UnitTests
+{
+    public class ScriptCollectionTests
+    {
+        private const string SqlLiteral = "select * from [Test]";
+
+        private readonly Assembly assembly;
+
+
+        public ScriptCollectionTests()
+        {
+#if NETCOREAPP
+            assembly = GetType().GetTypeInfo().Assembly;
+#else
+            assembly = Assembly.GetExecutingAssembly();
+#endif
+        }
+
+        [Fact]
+        [Trait("Category", "unit")]
+        public void LoadsAllScriptsFromAssembly()
+        {
+            var collection = new SqlScriptCollection();
+            collection.Add.FromAssembly(assembly, "Dapper.Scripts.Tests.Scripts");
+            var sql = collection.GetScriptSql("SqlTest.sql");
+            Assert.Equal(SqlLiteral, sql.Trim());
+        }
+
+        [Fact]
+        [Trait("Category", "unit")]
+        public async Task LoadsAllScriptsFromAssemblyAsync()
+        {
+            var collection = new SqlScriptCollection();
+            await collection.Add.FromAssemblyAsync(assembly, "Dapper.Scripts.Tests.Scripts");
+            var sql = collection.GetScriptSql("SqlTest.sql");
+            Assert.Equal(SqlLiteral, sql.Trim());
+        }
+
+        [Fact]
+        [Trait("Category", "unit")]
+        public void LoadsAllScriptsFromAssemblyWithEncoding()
+        {
+            var collection = new SqlScriptCollection();
+            collection.Add.FromAssembly(assembly, "Dapper.Scripts.Tests.Scripts", Encoding.UTF8);
+            var sql = collection.GetScriptSql("SqlTest.sql");
+            Assert.Equal(SqlLiteral, sql.Trim());
+        }
+
+        [Fact]
+        [Trait("Category", "unit")]
+        public void LoadsAllScriptsFromDirectory()
+        {
+            var collection = new SqlScriptCollection();
+            collection.Add.FromDirectory(".\\Scripts");
+            var sql = collection.GetScriptSql("SqlTest.sql");
+            Assert.Equal(SqlLiteral, sql.Trim());
+        }
+
+        [Fact]
+        [Trait("Category", "unit")]
+        public void LoadsAllScriptsFromFile()
+        {
+            var collection = new SqlScriptCollection();
+            collection.Add.FromFile(".\\Scripts\\SqlTest.sql");
+            var sql = collection.GetScriptSql("SqlTest.sql");
+            Assert.Equal(SqlLiteral, sql.Trim());
+        }
+    }
+}
---
+++ Dapper.Scripts.Tests/UnitTests/ScriptTransformTests.cs
@@ -0,0 +1,30 @@
+﻿using Dapper.Scripts.Collection;
+using Xunit;
+
+namespace Dapper.Scripts.Tests.UnitTests
+{
+    public class ScriptTransformTests
+    {
+        private const string SqlLiteral = "select * from [Test]";
+
+        private readonly object _params;
+
+
+        public ScriptTransformTests()
+        {
+            _params = new {
+                TableName = "[Test]",
+            };
+        }
+
+        [Fact]
+        [Trait("Category", "unit")]
+        public void ScriptReplacesTags()
+        {
+            var collection = new SqlScriptCollection();
+            collection.Add.FromDirectory(".\\Scripts");
+            var sql = collection.GetScriptSql("SqlTransformTest.sql", _params);
+            Assert.Equal(SqlLiteral, sql.Trim());
+        }
+    }
+}
