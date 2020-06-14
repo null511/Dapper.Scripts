8b5985eaa3e6c890df785467c7ba6c67459b427b
NULL511 <null511@outlook.com>
2020-06-14 17:45:31 -0400

    no mono again; github push

--- Usage.cs
+++ Usage.cs
@@ -2,19 +2,24 @@
 
 namespace Dapper.Scripts
 {
-    internal static class Usage
+    internal class UsageExample
     {
         public static SqlScriptConnectionFactory DatabaseName {get;}
 
 
-        static Usage()
+        static UsageExample()
         {
-            var assembly = Assembly.GetExecutingAssembly();
             var scripts = new SqlScriptCollection();
-            scripts.Add.FromAssembly(assembly, "namespace.Sql.");
+            scripts.Add.FromAssembly("namespace.Sql.");
 
             DatabaseName = new SqlScriptConnectionFactory(scripts);
             DatabaseName.ConnectionString = "Server: xxx;";
+        }
+
+        public Task DeleteAsync(long id)
+        {
+            using var connection = DatabaseName.Connect();
+            return connection.ExecuteScriptAsync("Example.Delete.sql", new {id});
         }
     }
 }
